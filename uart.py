ceb65f4b6296cec0bde58d29d5c605509d03b75c
volo <vsp.dmmi@gmail.com>
2022-12-01 12:24:01 -0500

    parser

--- uart.py
+++ uart.py
@@ -135,14 +135,14 @@ class Uart:
     def readLN(self):
         # need re = self.currentPort.readline().decode('ascii')
         # UnicodeDecodeError: 'ascii' codec can't decode byte 0xfe in position 5: ordinal not in range(128)
-        rend = self.currentPort.readline()
-        print(rend)
-        ree = b''
-        for ch in rend:
+        #rend = self.currentPort.readline()
+        #print(rend)
+        #ree = b''
+        #for ch in rend:
             #print(ch)
-            if int(ch) <= 128:
-                num_bytes = int(ch).to_bytes(1, byteorder='big')
-                ree = ree + num_bytes
-        #re = self.currentPort.readline().decode('ascii')
-        re = ree
+        #    if int(ch) <= 128:
+        #        num_bytes = int(ch).to_bytes(1, byteorder='big')
+        #        ree = ree + num_bytes
+        re = self.currentPort.readline().decode('ascii')
+        #re = ree
         return re
